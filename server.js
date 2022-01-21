c4696731fcdd7eed8e9ba377ac148fa21d7daf44
AntonBaltser <antonbaltser@yahoo.com>
2022-01-22 02:20:03 +0300

    nacht

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const cookieParser = require('cookie-parser')
 const fs = require('fs')
 
+let Minio = require('minio')
+// const minioClient = new Minio.Client({
+//     endPoint: 'play.min.io',
+//     port: 9000,
+//     useSSL: true,
+//     accessKey: 'Q3AM3UQ867SPQQA43P2F',
+//     secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
+// });
+var minioClient = new Minio.Client({
+    endPoint: 'play.min.io',
+    port: 9000,
+    useSSL: true,
+    accessKey: 'AKIAQVKU5JSQXKWLGRMV',
+    secretKey: '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'
+})
 const kachPassword = require('./src/modules/kashPassword')
 const cacheCreator = require('./src/modules/cacheCreator')
 
@@ -15,12 +30,12 @@ app.use(express.json());
 app.use(express.urlencoded());
 let token = cacheCreator()
 
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = '0';
+// process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = '0';
 
 app.all('/*', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
@@ -33,11 +48,26 @@ let server = app.listen(4000, (req, res) =>{
         console.log("Сервер рабтает по адрессу http://%s:%s", host, port)
 })
 
+var file = '/kart.jpg'
 
+// Make a bucket called europetrip.
+minioClient.makeBucket('europetrip', 'us-east-1', function(err) {
+    if (err) return console.log(err)
 
+    console.log('Bucket created successfully in "us-east-1".')
+
+    var metaData = {
+        'Content-Type': 'application/octet-stream',
+        'X-Amz-Meta-Testing': 1234,
+        'example': 5678
+    }
+    // Using fPutObject API upload your file to the bucket europetrip.
+    minioClient.fPutObject('europetrip', 'photos-europe.tar', file, metaData, function(err, etag) {
+        if (err) return console.log(err)
+        console.log('File uploaded successfully.')
+    });
+});
 app.post('/verificationName',async (req, res) => {
-
-
 
     const verificationName = await  Users.findOne({
             where: {
@@ -76,41 +106,44 @@ console.log(req.body)
 
 
     // const data = fs.readFileSync('./kart.jpg');
-    var data =  Buffer.from(JSON.stringify(req.body), 'base64')
+    let data =  Buffer.from(JSON.stringify(req.body), 'base64')
     // let blob = Buffer.from(JSON.stringify(req.body), 'base64').toString('ascii')
     // console.log(blob)
     // let data = blob
-    fs.createWriteStream('./static/upload/map11').write(data,() => console.dir('записанн файл'));
+    fs.createWriteStream('./static/upload/map1g1').write(data,() => console.dir('записанн файл'));
 res.send('alles ok')
     // fs.createWriteStream('./static/upload/map1.jpeg').write(blob, 'utf-8',() => console.dir('записанн файл'));
 })
 
-
-
-
 app.post('/auth',  async (req, res) =>{
     let body = req.body
     console.log(body)
+    let login = '';
     const loginAuth =
     await Users.findOne({
             where: {
                 'user_email': body.email,
                 'user_password': kachPassword(body.email, body.password)
             },
-        } )
-            .then(answer => answer !== null)
-            .then(isLoginAuth => isLoginAuth);
-    // loginAuth
+        })
+            .then(answer => {
+                login = answer?.user_name || 'no_login'
+                return answer !== null
+            })
+            .then(isLoginAuth => {
+                console.log(isLoginAuth)
+                return isLoginAuth
+            });
     if(loginAuth === true) {
 
  res.cookie('token', token);
+ res.cookie('login', login);
 
-            console.dir(token)
-        console.log('Cookies: ', req.cookies)
-        console.log('Signed Cookies: ', req.signedCookies)
-        res.send("ok")
+        // console.log('Cookies: ', req.cookies)
+        // console.log('Signed Cookies: ', req.signedCookies)
+        res.send(login)
     }
-    else res.send('пошел нахер пидр!')
+    else res.send(login)
 
    })
 
